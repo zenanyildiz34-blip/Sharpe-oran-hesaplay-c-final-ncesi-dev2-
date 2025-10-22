639b1eff807ef6b34e03a3874090594e590d09dc
zenanyildiz34-blip <zenanyildiz34@gmail.com>
2025-10-22 13:21:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-/*sharp oranını hesaplayan bir kod yazacağız bunun için kullanıcıdan 3 değer alacağız sonrasında sharp
- oranı formülüne göre sharp oranını hesaplayacağız-yanlış karakterler girilmesi durumunda hata komutu ile karşılaşacagız-
+/*sharpe oranını hesaplayan bir kod yazacağız bunun için kullanıcıdan 3 değer alacağız sonrasında sharpe
+ oranı formülüne göre sharpe oranını hesaplayacağız-yanlış karakterler girilmesi durumunda hata komutu ile karşılaşacagız-
  ve çıkan sonucu getirdiği kazanca göre yorumlayacagız*/
 
 #include <stdio.h>  //scanf,printf gibi fonksiyonları kullanmamızı sağlar
@@ -26,14 +26,16 @@ int main(void) { //c dilinde main fonksiyonu kodu başlatır
     }
     clearInputBuffer();
 
-    printf("Risksiz faiz orani (%% cinsinden): ");  //risksiz faiz oranını ister-bir nevi garantili kazanç kaynaklaırdır-yine aynı şekilde sayı şeklinde karakter girişi ister aksi halde hata verir
+    printf("Risksiz faiz orani (%% cinsinden): ");  /*risksiz faiz oranını ister-bir nevi garantili kazanç kaynaklaırdır-
+                                                     yine aynı şekilde sayı şeklinde karakter girişi ister aksi halde hata verir*/
     if (scanf("%lf", &risksiz_faiz) != 1) {
         printf("Gecersiz giris! Lütfen sayi giriniz.\n");
         return 1;
     }
     clearInputBuffer();
 
-    printf("Portfoyun standart sapmasi (%% cinsinden): ");  //portfoyun standrt sapması ise yatırımın ne kadar dalgalandığını ifade eder yüksekse yatırım çok oynak düşükse oynak olmadığını ıfade eder
+    printf("Portfoyun standart sapmasi (%% cinsinden): ");  /*portfoyun standrt sapması ise yatırımın ne kadar dalgalandığını 
+                                                          ifade eder yüksekse yatırım çok oynak düşükse oynak olmadığını ıfade eder*/
     if (scanf("%lf", &standart_sapma) != 1) {
         printf("Gecersiz giris! Lütfen sayi giriniz.\n");
         return 1;
@@ -41,15 +43,18 @@ int main(void) { //c dilinde main fonksiyonu kodu başlatır
     clearInputBuffer();
 
     // Negatif veya sıfır standart sapma kontrolü
-    if (standart_sapma <= 0) {  //formüle göre getiriden risksiz olanı çıkarıp sonucu standart sapmaya bölüyoruz matematiksel açıdan hata çıkmaması adına standart sapma 0 olamaz ayrıca gerçek hayatta her yatırım bir miktarda olsa risk içerir yanı 0dan küçük olamaz
+    if (standart_sapma <= 0) {                             /*formüle göre getiriden risksiz olanı çıkarıp sonucu standart sapmaya 
+                                                            bölüyoruz matematiksel açıdan hata çıkmaması adına standart sapma 0 olamaz ayrıca gerçek hayatta
+                                                           her yatırım bir miktarda olsa risk içerir yanı 0dan küçük olamaz*/
         printf("Standart sapma 0 veya negatif olamaz.\n"); //yanlı karakter girişlerinde ekranda uyarı verir
         return 1;
     }
 
     // Sharpe oranı hesaplama
-    sharpe_orani = (yatirim_getirisi - risksiz_faiz) / standart_sapma;  //sharp oranı formülümüz
+    sharpe_orani = (yatirim_getirisi - risksiz_faiz) / standart_sapma;  //sharpe oranı formülümüz
 
-    printf("\nSharpe Orani = %.2f\n", sharpe_orani);  // formülden çıkan sonçlarımız ondalıklı olabilir bu yüzden virgülden sonraki iki sayıyı görmek için bu satırı yazdık
+    printf("\nSharpe Orani = %.2f\n", sharpe_orani);  /*formülden çıkan sonçlarımız ondalıklı olabilir bu yüzden virgülden
+                                                        sonraki iki sayıyı görmek için bu satırı yazdık*/
 
     // son olarak Yorumsal değerlendirme yapıyoruz
     if (sharpe_orani < 0)
